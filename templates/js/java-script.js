bc7faefffa7ddc6b3432e09fac061a8981e02e8d
J A <kuba777@gmail.com>
2023-12-28 23:40:38 -0500

    Added reference links to js code

--- templates/js/java-script.js
+++ templates/js/java-script.js
@@ -1,15 +1,19 @@
 $(function() {
 
-    // Generate array from pythone list of lists
+    // https://stackoverflow.com/questions/7177029/getting-the-index-of-the-selected-item-in-jquery-auto-complete
+    // https://api.jqueryui.com/autocomplete/#option-source
+
+    // Generate array from Python list of lists
+    // label -> item_pn; value -> location
     var item_bin_array = [
         {% for entry in bin_list %}
-        {label:"{{ entry[0] }}", idx:"{{ entry[1] }}" },
+        {label:"{{ entry[0] }}", value:"{{ entry[1] }}" },
         {% endfor %}
     ];
 
-    // Autocompleate form with filing out location field
+    // Autocomplete form with filing out location field
     $( "#part_number" ).autocomplete({
         source: item_bin_array,
-        select: function(event, ui) { $('#location').val(ui.item.idx) ; }
+        select: function(event, ui) { $('#location').val(ui.item.value) ; }
     });
 });
